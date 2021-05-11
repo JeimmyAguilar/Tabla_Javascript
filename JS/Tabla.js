2be8b1e5093e71f4ab2ba78d989a5d6818629c6f
unknown <ajeimmy.1127@gmail.com>
2021-05-11 15:25:12 -0500

    Tarea

---
+++ JS/Tabla.js
@@ -0,0 +1,81 @@
+for (let i = 0; i <= 0; i++) {
+    let card = `<div class="m-3 card" style="width: 50rem;">
+    <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Lastname</th>
+        <th scope="col">Last</th>
+        <th scope="col">Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">2</th>
+        <td>Beltran</td>
+        <td>Otto</td>
+        <td>@Yelipssa</td>
+      </tr>
+      <tr>
+        <th scope="row">4</th>
+        <td>Patiño</td>
+        <td>Thornton</td>
+        <td>@Gerardo</td>
+      </tr>
+      <tr>
+        <th scope="row">6</th>
+        <td>Moreno</td>
+        <td>the Bird</td>
+        <td>@Marlon</td>
+      </tr>
+      <tr>
+      <th scope="row">8</th>
+      <td>Morales</td>
+      <td>Otto</td>
+      <td>@Kimberly</td>
+    </tr> 
+    <tr>
+    <th scope="row">10</th>
+    <td>Cardenas</td>
+    <td>Otto</td>
+    <td>@Dilan</td>
+      </tr>
+      <tr>
+     <th scope="row">12</th>
+  <td>Cocuy</td>
+  <td>Otto</td>
+  <td>@Michael</td>
+  </tr>
+  <tr>
+  <th scope="row">14</th>
+  <td>Aguilar</td>
+  <td>Otto</td>
+  <td>@Cesar</td>
+  </tr>
+  <tr>
+  <th scope="row">16</th>
+  <td>Aguilar</td>
+  <td>Otto</td>
+  <td>@Mersy</td>
+  </tr>
+  <tr>
+  <th scope="row">18</th>
+  <td>Aguilar</td>
+  <td>Otto</td>
+  <td>@Juan</td>
+  </tr>
+  <tr>
+  <th scope="row">20</th>
+  <td>Aguilar</td>
+  <td>Otto</td>
+  <td>@Camilo</td>
+  </tr> 
+      </tbody>
+    </table>
+    
+  </div>
+  </div>`;
+
+
+    document.getElementById("contenedor-tabla").innerHTML += card;
+}
